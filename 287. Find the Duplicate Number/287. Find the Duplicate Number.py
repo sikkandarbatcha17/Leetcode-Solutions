6d878f0b0e0a16625bd0946324a2410a12cb6f01
sikkandarbatcha m <46055863+sikkandarbatcha17@users.noreply.github.com>
2021-08-21 15:04:26 +0530

    Add files via upload

---
+++ 287. Find the Duplicate Number/287. Find the Duplicate Number.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def findDuplicate(self, nums):
+        
+        slow=nums[0]
+        fast=nums[0]
+        
+        slow=nums[slow]
+        fast=nums[nums[fast]]
+        while slow!=fast:
+            slow=nums[slow]
+            fast=nums[nums[fast]]
+        
+        fast=nums[0]
+        
+        while(slow!=fast):
+            slow=nums[slow]
+            fast=nums[fast]
+            
+        return slow
